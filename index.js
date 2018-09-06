7818ea1e3b7635fb16797db4e474f1a94fffcd7e
voltrue2 <voltrue2@gmail.com>
2018-09-06 11:45:04 +0900

    index.js was unintentially overwritten but fixed now...

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
-
-var linter = require('./linter');
-
-module.exports = function __lintIndex(path, packagePath, ignorelist, cb) {
-    linter.start(path, packagePath, ignorelist, function __lintIndexOnStart(error) {
-        if (error) {
-            return cb(error);
-        }
-        cb();
-    });
+const broker = require('./lib/broker');
+module.exports = {
+    start: broker.start,
+    stop: broker.stop,
+    info: broker.info,
+    onUpdate: broker.onUpdate,
+    onUpdated: broker.onUpdated,
+    setValue: broker.setValue,
+    getNodeValue: broker.getNodeValue,
+    getNodeEndPoints: broker.getNodeEndPoints,
+    handler: broker.handler,
+    send: broker.send
 };
