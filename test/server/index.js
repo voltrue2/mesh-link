f445cde2b0fd687c8ce153833241e833e8342491
voltrue2 <voltrue2@gmail.com>
2019-06-21 11:27:55 +0900

    Bug fix: non-multi configurations got broken

--- test/server/index.js
+++ test/server/index.js
@@ -98,6 +98,10 @@ function onListening() {
                 master: { host: '127.0.0.1', port: 6379 },
                 slave: { host: 'localhost', port: 6379 }
             }
+            /*
+            host: '127.0.0.1',
+            port: 6379
+            */
         },
         backups: {
             TypeTest: 3,
